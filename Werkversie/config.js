e06cefeb2fd3bfc79d6ced268af3ab17d8eed408
Linda van den Brink <l.vandenbrink@geonovum.nl>
2018-12-11 15:31:48 +0100

    Update config.js

--- Werkversie/config.js
+++ Werkversie/config.js
@@ -31,7 +31,7 @@ var respecConfig =
   format: "markdown",                 // altijd "markdown"     
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2017-06-14,  	    // Format is "YYY-MM-DD"
+  publishDate: "2017-06-14",  	    // Format is "YYY-MM-DD"
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors: 
