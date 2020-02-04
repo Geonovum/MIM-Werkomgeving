9df1f4de5755e2f240058fae62e135b728c4919d
Gelder <Jan.van.Gelder@alliander.com>
2020-02-04 16:30:32 +0100

    Versielog en config.js aangepast

--- config.js
+++ config.js
@@ -72,7 +72,7 @@ var respecConfig =
       companyURL: "https://www.geonovum.nl"
     },
     {
-      name:       "<br>Aan eerdere versies van MIM werkten mee:",
+      name:       "Aan eerdere versies van MIM werkten mee:",
       company:    "",
       companyURL: ""
     },
