1c6f0654bd5e915d5cb79e69a06dc2236d0c6db9
Jan van Gelder <43755492+JvGldr@users.noreply.github.com>
2021-06-07 11:01:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ var respecConfig =
   format: "markdown",                 // altijd "markdown"
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2021-06-04",  	      // Format is "YYYY-MM-DD"
+  //publishDate: "2021-06-04",  	      // Format is "YYYY-MM-DD"
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
