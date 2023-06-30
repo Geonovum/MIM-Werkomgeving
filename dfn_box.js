6f191d60cf39b7f509ec146c9eb98e702a0aa628
Gtrouborst <36916421+Gtrouborst@users.noreply.github.com>
2023-06-30 22:39:33 +0200

    add extra option

--- dfn_box.js
+++ dfn_box.js
@@ -12,16 +12,27 @@
 //   nodeList[i].style.backgroundColor = "red";
 // }
 
+// console.log("THIS IS A TEST");
+
 // OPTION 2
+
 // Wait until resources have loaded, including ReSpec
 document.addEventListener("load", function () {
   document.respec.ready.then(function () {
+    console.log("hello");
     // Do things
-    console.log("This is a TEST!!!")
-    // const collection = document.getElementsByClassName("definition");
-
-    // for (let i = 0; i < collection.length; i++) {
-    //   collection[i].style.backgroundColor = "red";
-    // };
   });
 });
+
+// OPTION 3
+// Wait until resources have loaded, including ReSpec
+// document.addEventListener("load", function () {
+//   document.respec.ready.then(function () {
+//     // Do things
+//     const collection = document.getElementsByClassName("definition");
+
+//     for (let i = 0; i < collection.length; i++) {
+//       collection[i].style.backgroundColor = "red";
+//     };
+//   });
+// });
