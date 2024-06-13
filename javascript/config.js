e68e9abcc247e1a2978d31fa3e93174e3aa3a0ee
Paul Janssen <p.janssen@geonovum.nl>
2024-06-13 17:10:29 +0200

    vastgestelde versie
    en issue #492 gefikst.

--- javascript/config.js
+++ javascript/config.js
@@ -6,8 +6,8 @@ let respecConfig = {
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   //specStatus:   "wv",               // Werkversie
   // specStatus: "cv",               // Consultatieversie
-  specStatus: "vv",               // Versie ter vaststelling
-  //specStatus: "def",              // Vastgestelde versie
+  //specStatus: "vv",               // Versie ter vaststelling
+  specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                 // Norm
@@ -29,13 +29,13 @@ let respecConfig = {
   shortName: "mim",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2024-04-08",
+  publishDate: "2024-06-13",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2023-10-18",
-  previousMaturity: "cv",
+  previousPublishDate: "2024-04-08",
+  previousMaturity: "vv",
   
   //neem hier de URL van de github repository op waar het respec document in staat
   github: "https://github.com/Geonovum/MIM-Werkomgeving",
